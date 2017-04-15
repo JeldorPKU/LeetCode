a27a3ec08d99cd5f97fe39ad6a91ca8d2a29c7e1
JeldorPKU <jack.jiang.capu@gmail.com>
2017-04-15 15:16:01 +0800

    Apr 15.

---
+++ P0292/Nim-Game.cpp
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Nim-Game.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/14/2017 22:51:20
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 != 0;
+    }
+};
